0ed3b426051cc333f599a8cf88eac6a16f658e0c
Alessio Perugini <alessioper.98@gmail.com>
2020-05-23 16:07:26 +0200

    DoBattle (incomplete)

--- engine.go
+++ engine.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strings"
 )
 
 type TechParam struct {
@@ -191,7 +192,7 @@ func InitBattleDefenders(d []Slot, dnum int, objs int) []Unit {
 	return u
 }
 
-func UniShoot(a *Unit, aweap int, b *Unit, absorbed, dm, dk *uint64) int64 {
+func UnitShoot(a Unit, aweap int, b *Unit, absorbed, dm, dk *uint64) int64 {
 	var prc, depleted float64
 	var apower, adelta int64
 
@@ -274,25 +275,25 @@ func WipeExploded(slot *[]Unit, amount int) int {
 	return exploded
 }
 
-func CheckFastDraw(aunits []Unit, aobjs int, dunits []Unit, dobjs int) int {
+func CheckFastDraw(aunits []Unit, aobjs int, dunits []Unit, dobjs int) bool {
 	for i := 0; i < aobjs; i++ {
 		if aunits[i].hull != aunits[i].hullmax {
-			return 0
+			return false
 		}
 	}
 
 	for i := 0; i < dobjs; i++ {
 		if dunits[i].hull != dunits[i].hullmax {
-			return 0
+			return false
 		}
 	}
 
-	return 1
+	return true
 }
 
 //TODO check rune on slot forse da int a rune boh
 //TODo check ptr pointer
-func GenSlot(ptr *string, units []Unit, slot, objnum int, a, d []Slot, attacker, techs bool) *string {
+func GenSlot(ptr strings.Builder, units []Unit, slot, objnum int, a, d []Slot, attacker, techs bool) strings.Builder {
 	var s []Slot
 	if attacker {
 		s = a
@@ -318,100 +319,312 @@ func GenSlot(ptr *string, units []Unit, slot, objnum int, a, d []Slot, attacker,
 
 	if techs {
 		if attacker { //TODO check %i
-			*ptr += fmt.Sprintf("i:%i;a:22:{", slot)
+			ptr.WriteString(fmt.Sprintf("i:%i;a:22:{", slot))
 		} else {
-			*ptr += fmt.Sprintf("i:%i;a:30:{", slot)
+			ptr.WriteString(fmt.Sprintf("i:%i;a:30:{", slot))
 		}
 	} else {
 		if attacker { //TODO check %i
-			*ptr += fmt.Sprintf("i:%i;a:19:{", slot)
+			ptr.WriteString(fmt.Sprintf("i:%i;a:19:{", slot))
 		} else {
-			*ptr += fmt.Sprintf("i:%i;a:27:{", slot)
+			ptr.WriteString(fmt.Sprintf("i:%i;a:27:{", slot))
 		}
 	}
 
-	*ptr += fmt.Sprintf("s:4:\"name\";s:%i:\"%s\";", len(s[slot].name), s[slot].name)
-	*ptr += fmt.Sprintf("s:2:\"id\";i:%i;", s[slot].id)
-	*ptr += fmt.Sprintf("s:1:\"g\";i:%i;", s[slot].g)
-	*ptr += fmt.Sprintf("s:1:\"s\";i:%i;", s[slot].s)
-	*ptr += fmt.Sprintf("s:1:\"p\";i:%i;", s[slot].p)
+	ptr.WriteString(fmt.Sprintf("s:4:\"name\";s:%i:\"%s\";", len(s[slot].name), s[slot].name))
+	ptr.WriteString(fmt.Sprintf("s:2:\"id\";i:%i;", s[slot].id))
+	ptr.WriteString(fmt.Sprintf("s:1:\"g\";i:%i;", s[slot].g))
+	ptr.WriteString(fmt.Sprintf("s:1:\"s\";i:%i;", s[slot].s))
+	ptr.WriteString(fmt.Sprintf("s:1:\"p\";i:%i;", s[slot].p))
 
 	if techs {
-		*ptr += fmt.Sprintf("s:4:\"weap\";i:%i;", s[slot].weap)
-		*ptr += fmt.Sprintf("s:4:\"shld\";i:%i;", s[slot].shld)
-		*ptr += fmt.Sprintf("s:4:\"armr\";i:%i;", s[slot].armor)
+		ptr.WriteString(fmt.Sprintf("s:4:\"weap\";i:%i;", s[slot].weap))
+		ptr.WriteString(fmt.Sprintf("s:4:\"shld\";i:%i;", s[slot].shld))
+		ptr.WriteString(fmt.Sprintf("s:4:\"armr\";i:%i;", s[slot].armor))
 	}
 
 	for n := 0; n < 14; n++ {
-		*ptr += fmt.Sprintf("i:%i;i:%i;", 202+n, coll.fleet[n])
+		ptr.WriteString(fmt.Sprintf("i:%i;i:%i;", 202+n, coll.fleet[n]))
 	}
 
 	if !attacker {
 		for n := 0; n < 8; n++ {
-			*ptr += fmt.Sprintf("i:%i;i:%i;", 401+n, coll.def[n])
+			ptr.WriteString(fmt.Sprintf("i:%i;i:%i;", 401+n, coll.def[n]))
 		}
 	}
 
-	*ptr += fmt.Sprintf("}")
+	ptr.WriteString(fmt.Sprintf("}"))
 	return ptr
 }
 
-func RapidFire(atyp, dtyp int) int {
-	rapidfire := 0
+func RapidFire(atyp, dtyp int) bool {
+	rapidfire := false
 
 	if atyp > 400 {
-		return 0
+		return false
 	}
 
 	if atyp == 214 && (dtyp == 210 || dtyp == 212) && MyRand(1, 10000) > 8 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp != 210 && (dtyp == 210 || dtyp == 212) && MyRand(1, 100) > 20 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 205 && dtyp == 202 && MyRand(1, 100) > 33 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 206 && dtyp == 204 && MyRand(1, 1000) > 166 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 206 && dtyp == 401 && MyRand(1, 100) > 10 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 211 && (dtyp == 401 || dtyp == 402) && MyRand(1, 100) > 20 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 211 && (dtyp == 403 || dtyp == 405) && MyRand(1, 100) > 10 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 213 && dtyp == 215 && MyRand(1, 100) > 50 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 213 && dtyp == 402 && MyRand(1, 100) > 10 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 215 && (dtyp == 202 || dtyp == 203) && MyRand(1, 100) > 20 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 215 && (dtyp == 205 || dtyp == 206) && MyRand(1, 100) > 25 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 215 && dtyp == 207 && MyRand(1, 1000) > 143 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && (dtyp == 202 || dtyp == 203 || dtyp == 208 || dtyp == 209) && MyRand(1, 1000) > 4 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 204 && MyRand(1, 1000) > 5 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 205 && MyRand(1, 1000) > 10 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 206 && MyRand(1, 1000) > 30 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 207 && MyRand(1, 1000) > 33 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 211 && MyRand(1, 1000) > 40 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 213 && MyRand(1, 1000) > 200 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 215 && MyRand(1, 1000) > 66 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && (dtyp == 401 || dtyp == 402) && MyRand(1, 1000) > 5 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && (dtyp == 403 || dtyp == 405) && MyRand(1, 1000) > 10 {
-		rapidfire = 1
+		rapidfire = true
 	} else if atyp == 214 && dtyp == 404 && MyRand(1, 1000) > 20 {
-		rapidfire = 1
+		rapidfire = true
 	}
 
 	return rapidfire
+}
+
+func DoBattle(a []Slot, anum int, d []Slot, dnum int) int {
+	var aobjs, dobjs, idx, rounds int64
+	var apower, atyp, dtyp int64
+	var fastdraw, rapidfire bool
+	var aunits, dunits []Unit
+	var unit Unit
+	//var ptr = ResultBuffer //TODO optimize this buffer
+	var res string //TODO capire a cosa serve questa var: round_patch string
+	var dm, dk uint64
+	var ptr strings.Builder
+
+	shoots := make([]uint64, 2)
+	spower := make([]uint64, 2)
+	absorbed := make([]uint64, 2)
+
+	for i := 0; i < anum; i++ {
+		for n := 0; n < 14; n++ {
+			aobjs += int64(a[i].fleet[n])
+		}
+	}
+	for i := 0; i < dnum; i++ {
+		for n := 0; n < 14; n++ {
+			dobjs += int64(d[i].fleet[n])
+		}
+		if i == 0 {
+			for n := 0; n < 8; n++ {
+				dobjs += int64(d[i].def[n])
+			}
+		}
+	}
+
+	aunits = InitBattleAttackers(a, anum, int(aobjs))
+	if len(aunits) == 0 {
+		return 0
+	}
+	dunits = InitBattleDefenders(d, dnum, int(dobjs))
+	if len(dunits) == 0 {
+		return 0
+	}
+
+	ptr.WriteString("a:5:{")
+	ptr.WriteString("s:6:\"before\";a:2:{")
+	ptr.WriteString(fmt.Sprintf("s:9:\"attackers\";a:%i:{", anum))
+	for slot := 0; slot < anum; slot++ {
+		ptr = GenSlot(ptr, aunits, slot, int(aobjs), a, d, true, true) //TODO uhm pointer stuff not sure
+	}
+	ptr.WriteString("}")
+	ptr.WriteString("a:5:{")
+	ptr.WriteString(fmt.Sprintf("s:9:\"defenders\";a:%i:{", dnum))
+
+	for slot := 0; slot < dnum; slot++ {
+		ptr = GenSlot(ptr, dunits, slot, int(dobjs), a, d, false, true)
+	}
+	ptr.WriteString("}")
+	ptr.WriteString("}")
+
+	//round_patch = ptr + 15 //TODO result buffer as rune?
+	ptr.WriteString("s:6:\"rounds\";a:X:{")
+
+	for rounds = 0; rounds < 6; rounds++ {
+		if aobjs == 0 || dobjs == 0 {
+			break
+		}
+
+		shoots[0] = 0
+		shoots[1] = 0
+		spower[0] = 0
+		spower[1] = 0
+		absorbed[0] = 0
+		absorbed[1] = 0
+
+		// �������� ����.
+		for i := 0; int64(i) < aobjs; i++ {
+			if aunits[i].exploded {
+				aunits[i].shield = 0
+				aunits[i].shieldmax = 0
+			} else {
+				aunits[i].shieldmax = fleetParam[aunits[i].obj_type-100].shield * (10 + int64(a[aunits[i].slot_id].shld)) / 10
+				aunits[i].shield = aunits[i].shieldmax
+			}
+		}
+		for i := 0; int64(i) < dobjs; i++ {
+			if dunits[i].exploded {
+				dunits[i].shield = 0
+				dunits[i].shieldmax = 0
+			} else {
+				if dunits[i].obj_type >= 200 {
+					dunits[i].shieldmax = defenseParam[dunits[i].obj_type-200].shield * (10 + int64(d[dunits[i].slot_id].shld)) / 10
+					dunits[i].shield = dunits[i].shieldmax
+				} else {
+					dunits[i].shieldmax = fleetParam[dunits[i].obj_type-100].shield * (10 + int64(d[dunits[i].slot_id].shld)) / 10
+					dunits[i].shield = dunits[i].shieldmax
+				}
+			}
+		}
+
+		for slot := 0; slot < anum; slot++ {
+			for i := 0; int64(i) < aobjs; i++ {
+				rapidfire = true
+				unit = aunits[i]
+				if int(unit.slot_id) == slot { //TODO check this
+					for rapidfire {
+						idx = int64(MyRand(0, int(dobjs-1)))
+						apower = UnitShoot(unit, a[slot].weap, &dunits[idx], &absorbed[1], &dm, &dk)
+						shoots[0]++
+						spower[0] += uint64(apower) //TODO check this
+
+						// ��������� ID � ������� ������, ����� ���� ��������.
+						atyp = int64(unit.obj_type) //TODO check this
+						if atyp < 200 {
+							atyp += 102
+						} else {
+							atyp += 201
+						}
+						dtyp = int64(dunits[idx].obj_type) //TODO check this
+						if dtyp < 200 {
+							dtyp += 102
+						} else {
+							dtyp += 201
+						}
+						rapidfire = RapidFire(int(atyp), int(dtyp))
+
+						if Rapidfire == 0 {
+							rapidfire = false
+						}
+					}
+				}
+			}
+		}
+		for slot := 0; slot < dnum; slot++ {
+			for i := 0; int64(i) < dobjs; i++ {
+				rapidfire = true
+				unit = dunits[i]
+				if int(unit.slot_id) == slot { //TODO check this
+					// �������.
+					for rapidfire {
+						idx = int64(MyRand(0, int(aobjs-1)))
+						apower = UnitShoot(unit, d[slot].weap, &aunits[idx], &absorbed[0], &dm, &dk)
+						shoots[1]++
+						spower[1] += uint64(apower)
+
+						atyp = int64(unit.obj_type) //TODO check this
+						if atyp < 200 {
+							atyp += 102
+						} else {
+							atyp += 201
+						}
+						dtyp = int64(aunits[idx].obj_type) //TODO check this
+						if dtyp < 200 {
+							dtyp += 102
+						} else {
+							dtyp += 201
+						}
+						rapidfire = RapidFire(int(atyp), int(dtyp))
+
+						if Rapidfire == 0 {
+							rapidfire = false
+						}
+					}
+				}
+			}
+		}
+
+		fastdraw = CheckFastDraw(aunits, int(aobjs), dunits, int(dobjs))
+
+		aobjs -= int64(WipeExploded(&aunits, int(aobjs)))
+		dobjs -= int64(WipeExploded(&dunits, int(dobjs)))
+
+		// Round.
+		ptr.WriteString(fmt.Sprintf("i:%i;a:8:", rounds))
+		ptr.WriteString(fmt.Sprintf("{s:6:\"ashoot\";d:%d;", int64(shoots[0])))
+		ptr.WriteString(fmt.Sprintf("s:6:\"apower\";d:%d;", int64(spower[0])))
+		ptr.WriteString(fmt.Sprintf("s:7:\"dabsorb\";d:%d;", int64(absorbed[1])))
+		ptr.WriteString(fmt.Sprintf("s:6:\"dshoot\";d:%d;", int64(shoots[1])))
+		ptr.WriteString(fmt.Sprintf("s:6:\"dpower\";d:%d;", int64(spower[1])))
+		ptr.WriteString(fmt.Sprintf("s:7:\"aabsorb\";d:%d;", int64(absorbed[0])))
+		ptr.WriteString(fmt.Sprintf("s:9:\"attackers\";a:%i:{", anum))
+		for slot := 0; slot < anum; slot++ {
+			ptr = GenSlot(ptr, aunits, slot, int(aobjs), a, d, true, false)
+		}
+		ptr.WriteString("}")
+		ptr.WriteString(fmt.Sprintf("s:9:\"defenders\";a:%i:{", dnum))
+		for slot := 0; slot < dnum; slot++ {
+			ptr = GenSlot(ptr, dunits, slot, int(dobjs), a, d, false, false)
+		}
+		ptr.WriteString("}")
+		ptr.WriteString("}")
+
+		if fastdraw {
+			rounds++
+			break
+		}
+	}
+
+	//TODO capire questo
+	//*round_patch = '0' + (rounds);
+
+	if aobjs > 0 && dobjs == 0 {
+		res = "awon"
+	} else if dobjs > 0 && aobjs == 0 {
+		res = "dwon"
+	} else {
+		res = "draw"
+	}
+
+	ptr.WriteString(fmt.Sprintf("}s:6:\"result\";s:4:\"%s\";", res))
+	ptr.WriteString(fmt.Sprintf("s:2:\"dm\";d:%d;", int64(dm)))
+	ptr.WriteString(fmt.Sprintf("s:2:\"dk\";d:%d;}", int64(dk)))
+
+	return 1
 }
 
 func main() {
